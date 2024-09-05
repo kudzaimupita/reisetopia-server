07817abca0073b32bdf32a0d2ac11d8b5a961225
kudzaimupita <kmupita@gmail.com>
2024-09-05 14:33:16 +0200

    chore: add test for helathcheck

---
+++ src/api/healthCheck/__tests__/healthCheckRouter.test.ts
@@ -0,0 +1,15 @@
+import { StatusCodes } from "http-status-codes";
+import request from "supertest";
+import { app } from "../../../server";
+
+describe("Health Check API endpoints", () => {
+  it("GET / - success", async () => {
+    const response = await request(app).get("/health-check");
+    const result = response.body;
+
+    expect(response.statusCode).toEqual(StatusCodes.OK);
+    expect(result.success).toBeTruthy();
+    expect(result.result).toBeNull();
+    expect(result.message).toEqual("Service is healthy");
+  });
+});
