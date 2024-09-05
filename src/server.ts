844d7c27d79ba6593e51fce44f55c4a386f191c8
kudzaimupita <kmupita@gmail.com>
2024-09-05 11:18:21 +0200

    feat: add health check route

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,15 @@ import express, { type Express } from "express";
 import helmet from "helmet";
 import { pino } from "pino";
 import errorHandler from "./common/middleware/errorHandler";
+import { healthCheckRouter } from "./api/healthCheck/healthCheckRouter";
 
 const logger = pino({ name: "server start" });
 const app: Express = express();
 
 app.use(cors());
 app.use(helmet());
+
+app.use("/health-check", healthCheckRouter);
 
 app.use(errorHandler());
 export { app, logger };
